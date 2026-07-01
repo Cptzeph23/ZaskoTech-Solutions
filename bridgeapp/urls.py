943a3b6a4e2b98ed7ab9d441508569bab9413510
Cptzeph23 <asekazephaniah@gmail.com>
2026-07-01 16:29:40 +0300

    checks

--- bridgeapp/urls.py
+++ bridgeapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views, admin
 from django.contrib import admin
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('services/', views.services, name='services'),
     path('starter/', views.starter, name='starter'),
